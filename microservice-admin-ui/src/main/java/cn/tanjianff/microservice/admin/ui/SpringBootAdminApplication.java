581d878405bc34e7182c0f3bfb0fec2be0b48d27
WiselySugarFree <tanjian20150101@163.com>
2017-10-16 23:17:29 +0800

    添加admin管理控制页面——micrservice-admin-ui

---
+++ microservice-admin-ui/src/main/java/cn/tanjianff/microservice/admin/ui/SpringBootAdminApplication.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 重庆交通大学.信息科学与工程学院.
+ */
+
+package cn.tanjianff.microservice.admin.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@EnableAdminServer
+public class SpringBootAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminApplication.class, args);
+    }
+}
